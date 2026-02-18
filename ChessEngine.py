8761da1901b4e0eb0de748144e4a01a0c8ef3b63
Caden <cadensiberttech@gmail.com>
2026-02-18 05:28:40 -0600

    Initial game state added

---
+++ ChessEngine.py
@@ -0,0 +1,18 @@
+# This is where the data will be STORED, for storing all info about the
+# 'state' of the chess game. It'll also handle move logic, and it'll keep track of moves.
+
+# The constant state of the board.
+class GameState():
+    def __init__(self):
+        # List in a list, to represent the board for ease of use later on.
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove = True
+        self.movelog = []
---
+++ ChessMain.py
@@ -0,0 +1,64 @@
+# Main 'driver' file, this is where user input and where
+# displaying GameState Object will occur.
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+colors = ["w", "b"]
+pieces = ["P", "R", "N", "B", "Q", "K"]
+
+# Load images only once -- This will initialize a global dict of the images
+def loadImages():
+    # Automate the loading of images
+    for color in colors:
+        for piece in pieces:
+            IMAGES[color + piece] = p.transform.scale(p.image.load(f'images/{color + piece}.png'), (SQ_SIZE, SQ_SIZE))
+
+# Main driver: Handles user input and updating graphics
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages() # Only done once
+    running = True
+    while running:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+# Draws all graphics in current game state
+def drawGameState(screen, gs):
+    drawBoard(screen) # This needs to be constantly refreshed so that we don't have duplicates on a board, keeps it up to date
+    drawPieces(screen, gs.board)
+
+
+def drawBoard(screen):
+    # Start at 0,0 and expand right, using SQ_SIZE for location iteration and dimension as the col and row iterator
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            if (row + col) % 2 == 0:
+                p.draw.rect(screen, p.Color("Gray"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            else:
+                p.draw.rect(screen, p.Color("Dark Green"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            if board[row][col] == "--":
+                break
+            else:
+                screen.blit(IMAGES[board[row][col]], (col * SQ_SIZE, row * SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
