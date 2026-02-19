42fd3d7ba5a7f7f022ff683465bdaadba1dc1847
Caden <cadensiberttech@gmail.com>
2026-02-18 18:56:28 -0600

    Added movement and some highlighting functionality

--- ChessEngine.py
+++ ChessEngine.py
@@ -16,3 +16,63 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
         self.whiteToMove = True
         self.movelog = []
+
+    # To make a move, we need the initial position, and the end position. With board.
+    # When moving a piece, there's two things that happen:
+    # 1. The piece leaves its original spot (making it empty)
+    # 2. The piece takes the space of the end move (so end is now where the piece is)
+    def makeMove(self, move):
+
+        if self.board[move.startRow][move.startCol] != "--":
+            self.board[move.startRow][move.startCol] = "--"
+            self.board[move.endRow][move.endCol] = move.pieceMoved
+            self.movelog.append(move)
+            self.whiteToMove = not self.whiteToMove
+
+
+class Move():
+
+    # key : value
+    # Top left is zero in list, but its 8 in chess notation
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+
+    # value : key
+    # Seperately grabs both values in the tuple, then at the end
+    # of each iteration, flips the 'key' (k) and 'value' (v)
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        
+    def getChessNotation(self):
+        """
+        Algebraic notation Symbols for later usage
+        TODO: FEN notation
+        x -> Piece taken
+        e.p. -> Piece taken en passant
+        + -> Check
+        # -> Checkmate
+        = -> Pawn promotion *
+        0-0 -> Castle King-side
+        0-0-0 -> Castle Queen-side
+        1-0 -> White win
+        ½-½ -> Draw
+        0-1 -> Black win
+        (=) -> Draw offered
+        """
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    # Helper function to return the Rank + File in chess notation
+    # Tempted to make this a class method just for the practice lol
+    def getRankFile(self, row, col):
+        return self.colsToFiles[col] + self.rowsToRanks[row]
+
+
--- ChessMain.py
+++ ChessMain.py
@@ -28,25 +28,48 @@ def main():
     gs = ChessEngine.GameState()
     loadImages() # Only done once
     running = True
+    sqselected = () # Keeps track of users clicks
+    playerclicks = [] # Two tuples, one for each click spot location (x,y)
+
     while running:
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
-        drawGameState(screen, gs)
+            elif event.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqselected == (row, col): # They selected the same piece (unclicking)
+                    sqselected = ()
+                    playerclicks = []
+                else:
+                    sqselected = (row, col)
+                    playerclicks.append(sqselected)
+
+                if len(playerclicks) == 2:
+                    move = ChessEngine.Move(playerclicks[0], playerclicks[1] , gs.board)
+                    gs.makeMove(move)
+                    playerclicks = []
+
+        drawGameState(screen, gs, sqselected)
         clock.tick(MAX_FPS)
         p.display.flip()
 
+
+
 # Draws all graphics in current game state
-def drawGameState(screen, gs):
-    drawBoard(screen) # This needs to be constantly refreshed so that we don't have duplicates on a board, keeps it up to date
+def drawGameState(screen, gs, sqselected):
+    drawBoard(screen, sqselected) # This needs to be constantly refreshed so that we don't have duplicates on a board, keeps it up to date
     drawPieces(screen, gs.board)
 
 
-def drawBoard(screen):
+def drawBoard(screen, sqselected):
     # Start at 0,0 and expand right, using SQ_SIZE for location iteration and dimension as the col and row iterator
     for row in range(DIMENSION):
         for col in range(DIMENSION):
-            if (row + col) % 2 == 0:
+            if (row, col) == sqselected:
+                p.draw.rect(screen, p.Color("Yellow"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            elif (row + col) % 2 == 0:
                 p.draw.rect(screen, p.Color("Gray"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
             else:
                 p.draw.rect(screen, p.Color("Dark Green"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
@@ -55,9 +78,7 @@ def drawBoard(screen):
 def drawPieces(screen, board):
     for row in range(DIMENSION):
         for col in range(DIMENSION):
-            if board[row][col] == "--":
-                break
-            else:
+            if board[row][col] != "--":
                 screen.blit(IMAGES[board[row][col]], (col * SQ_SIZE, row * SQ_SIZE))
 
 if __name__ == "__main__":
